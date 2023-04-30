e6b8588bde04b594c05493063f84f8933389b511
Lovis Rentsch <gpg@lovirent.eu>
2023-04-30 23:03:04 +0200

    added all commands

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 use std::process::Command;
+use colored::*;
+
 fn main() {
-    get_name();
-    get_temp();
+    let name = get_name();
+    println!("{name}");
+    // let temp = get_temp();
+    // println!("{temp}");
+    // let auslast = get_last();
+    // println!("{auslast}");
 }
-fn get_temp() {
+fn get_temp() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits");
-    get.status().expect("process failed to execute");
+    get.status().expect("process failed to execute").to_string()
 }
-fn get_name() {
+fn get_name() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=gpu_name --format=csv,noheader");
-    get.status().expect("process failed to execute");
+    get.status().expect("process failed to execute").to_string()
+}
+fn get_last() -> String {
+    let mut get = Command::new("sh");
+    get.arg("-c");
+    get.arg("nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader");
+    get.status().expect("process failed to execute").to_string()
 }

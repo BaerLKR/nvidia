ef27cee3797da44b608247cd124c9370cf812acd
Lovis Rentsch <gpg@lovirent.eu>
2023-04-30 23:42:53 +0200

    return values as tring (without status) (was a struggle lol)

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,52 @@ use std::process::Command;
 use colored::*;
 
 fn main() {
-    let name = get_name();
-    println!("{name}");
-    // let temp = get_temp();
-    // println!("{temp}");
-    // let auslast = get_last();
-    // println!("{auslast}");
+    println!("{}", get_name().yellow());
+    println!("{}", get_temp());
+    println!("{}", get_last());
 }
 fn get_temp() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits");
-    get.status().expect("process failed to execute").to_string()
+    let mut res = String::new();
+    match get.output() {
+        Ok(o) => {
+            res = String::from_utf8(o.stdout).expect("error translating");
+        },
+        Err(e) => {
+            println!("{e}");
+        }
+    }
+    res.trim().to_string()
 }
 fn get_name() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=gpu_name --format=csv,noheader");
-    get.status().expect("process failed to execute").to_string()
+    let mut res = String::new();
+    match get.output() {
+        Ok(o) => {
+            res = String::from_utf8(o.stdout).expect("error translating");
+        },
+        Err(e) => {
+            println!("{e}");
+        }
+    }
+    res.trim().to_string()
 }
 fn get_last() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader");
-    get.status().expect("process failed to execute").to_string()
+    let mut res = String::new();
+    match get.output() {
+        Ok(o) => {
+            res = String::from_utf8(o.stdout).expect("error translating");
+        },
+        Err(e) => {
+            println!("{e}");
+        }
+    }
+    res.trim().to_string()
 }

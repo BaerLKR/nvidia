cdddedc0c29d093ae2b7271993d29e56cefe88c0
Lovis Rentsch <gpg@lovirent.eu>
2023-05-01 13:45:18 +0200

    cleaned up the cmd crate

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,55 +1,31 @@
-use std::process::Command;
+use std::{process::Command};
 pub fn get_temp() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits");
-    let mut res = String::new();
-    match get.output() {
-        Ok(o) => {
-            res = String::from_utf8(o.stdout).expect("error translating");
-        },
-        Err(e) => {
-            println!("{e}");
-        }
-    }
-    res.trim().to_string()
+    format(get)
 }
 pub fn get_name() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=gpu_name --format=csv,noheader");
-    let mut res = String::new();
-    match get.output() {
-        Ok(o) => {
-            res = String::from_utf8(o.stdout).expect("error translating");
-        },
-        Err(e) => {
-            println!("{e}");
-        }
-    }
-    res.trim().to_string()
+    format(get)
 }
 pub fn get_last() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader");
-    let mut res = String::new();
-    match get.output() {
-        Ok(o) => {
-            res = String::from_utf8(o.stdout).expect("error translating");
-        },
-        Err(e) => {
-            println!("{e}");
-        }
-    }
-    res.trim().to_string()
+    format(get)
 }
 pub fn get_driver_version() -> String {
     let mut get = Command::new("sh");
     get.arg("-c");
     get.arg("nvidia-smi --query-gpu=driver_version --format=csv,noheader");
+    format(get)
+}
+fn format(mut i: Command) -> String {
     let mut res = String::new();
-    match get.output() {
+    match i.output() {
         Ok(o) => {
             res = String::from_utf8(o.stdout).expect("error translating");  
         },
